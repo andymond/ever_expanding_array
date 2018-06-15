8a9bcba104300481a93326376a56f8f5c32eb8f5
Andrew Dymond <andy.dymond@gmail.com>
2018-06-15 15:25:56 -0600

    Add it appends random number to end of array test

--- track_median_test.js
+++ track_median_test.js
@@ -10,4 +10,19 @@ describe('TrackMedian', () => {
     tm.sum.should.eq(0)
     tm.limit.should.eq(1)
   })
+
+  it('it appends random number to end of array', () => {
+    let tm = new TrackMedian(1);
+
+    tm.array.should.deep.eq([])
+    tm.append()
+
+    tm.array.length.should.eq(1)
+    tm.array[0].should.be.a('number')
+
+    tm.append()
+
+    tm.array.length.should.eq(2)
+    tm.array[1].should.be.a('number')
+  })
 })
