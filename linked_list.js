b0e1098b8d48036a5f09de0db127ae2c4ab3d4d5
Andrew Dymond <andy.dymond@gmail.com>
2018-06-15 17:06:27 -0600

    Add append method to linked list

--- linked_list.js
+++ linked_list.js
@@ -6,4 +6,19 @@ module.exports = class LinkedList {
     this.count = 0;
     this.sum = 0;
   }
+
+  append(value, current_node = this.head) {
+    if (this.head === null) {
+      this.head = new Node(value)
+      this.count += 1
+      this.sum += value
+    } else if (current_node.next_node === null) {
+      current_node.next_node = new Node(value)
+      this.count += 1
+      this.sum += value
+    } else {
+      current_node = current_node.next_node
+      this.append(value, current_node)
+    }
+  }
 }
