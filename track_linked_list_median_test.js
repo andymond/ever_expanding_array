b7781892d707a2253fef6a637f3705f508f3a1cb
Andrew Dymond <andy.dymond@gmail.com>
2018-06-15 17:09:11 -0600

    Remove sum & count methods from linked list

---
+++ track_linked_list_median_test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const should = chai.should();
+const track_median = ('./track_linked_list_median.js')
+
+describe('tracks linked list median', () => {
+  it('it can find linked list median at a certain node', () => {
+
+  })
+
+  it('it generates a linked list of random values', () => {
+
+  })
+
+  it('it generates a linked list of medians of those random values', () => {
+
+  })
+})
