1babd850fefc3e251848f5aaa26bac85f2de848f
Andrew Dymond <andy.dymond@gmail.com>
2018-06-15 19:21:59 -0600

    As it finds medians of linked list test

--- track_linked_list_median_test.js
+++ track_linked_list_median_test.js
@@ -1,17 +1,21 @@
 const chai = require('chai');
 const should = chai.should();
-const track_median = ('./track_linked_list_median.js')
+const expect = chai.expect;
+const TrackLinkedListMedian = require('./track_linked_list_median.js')
 
 describe('tracks linked list median', () => {
-  it('it can find linked list median at a certain node', () => {
+  it('it initializes with empty linked list', () => {
+    let tm = new TrackLinkedListMedian
 
+    tm.list.should.be.an('object')
+    expect(tm.list.head).to.be.null
   })
 
-  it('it generates a linked list of random values', () => {
+  it('it finds medians of linked list', () => {
+    let tm = new TrackLinkedListMedian(4)
 
-  })
+    let medians = tm.run()
 
-  it('it generates a linked list of medians of those random values', () => {
-
+    medians.head.next_node.next_node.next_node.data.should.eq(tm.list.sum()/tm.list.count())
   })
 })
